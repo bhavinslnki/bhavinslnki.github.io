5b3c90e9b52dd1c93d56293d524ccf32b1fd7b0f
bhavin-developer <solankibhavin2881@gmail.com>
2025-02-02 16:03:47 +0530

    changes for images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath:'my-portfolio',
   images: {
-    domains: ["ndshake.com.tw","flatquest.se","media.licdn.com","enjoyfresh.com","play-lh.googleusercontent.com","cdn.builder.io"],
+    domains: ["ndshake.com.tw"],
   },
 }
 
