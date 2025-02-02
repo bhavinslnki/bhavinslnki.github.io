697d22aa643f71049d57cf8c9a606be28e369927
bhavin-developer <solankibhavin2881@gmail.com>
2025-02-02 16:24:08 +0530

    changes for images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath:'/my-portfolio/',
+  // reactStrictMode: true,
+  output: "export", // Required for GitHub Pages
+  basePath:'/my-portfolio',
   assetPrefix: "/my-portfolio/",
 
   images: {
