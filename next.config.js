f28491ac8a38bb0dc0fbe64f3f944b32857ea580
bhavin-developer <solankibhavin2881@gmail.com>
2025-02-02 16:21:27 +0530

    changes for images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath:'/my-portfolio',
-  assetPrefix: "/my-portfolio",
+  basePath:'/my-portfolio/',
+  assetPrefix: "/my-portfolio/",
 
   images: {
     unoptimized: true,
